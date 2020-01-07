b0e3f901d38c2700fd7058b8f3694f10f64e2da5
StevenGoehler <steven@goehler.dk>
2020-01-07 21:28:14 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './css/view1.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="navBar">
+        <a href="" className="navLeft" id="fronLogo"></a>
+        <a href="" className="navLeft"></a>
+        <a href="" className="navRight"></a>
+      </div>
+      <div className="lightGreen">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
