fde07832364055952dcf2538ba93194237bd8bb8
Goehler <Steven.Goehler@itelligence.dk>
2020-02-25 20:32:45 +0100

    Added budle file and deploying is now possible with cf

--- truckchain-backend/main.js
+++ truckchain-backend/main.js
@@ -1,25 +1,25 @@
-const invoke = require('./invoke');
-const quary = require('./queryAllOrdersForOwner');
-const bodyParser = require('body-parser');
-const express = require('express');
+const invoke = require("./invoke");
+const quary = require("./queryAllOrdersForOwner");
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
 //Port for backend
-const port = 5000;
+const port = 8080;
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-
-app.get('/'); // serve index.html
-app.get('/api/test', (req, res) => res.send('Hello World!'));
+app.use(express.static("build"));
+app.get("/api/test", (req, res) => res.send("Hello World!"));
 
 // quary all orders to load into table returned in object -> payload
-app.get('/api/order/QuaryForOwner', async (req, res) => {
+app.get("/api/order/QuaryForOwner", async (req, res) => {
   try {
-    console.log('Trying');
-    const orderResult = await quary.quaryAllOrdersForOwner('Mads', 'Ulla');
+    console.log("Trying");
+    const orderResult = await quary.quaryAllOrdersForOwner("Mads", "Ulla");
     console.log(orderResult);
 
     res.send({
@@ -35,10 +35,10 @@ app.get('/api/order/QuaryForOwner', async (req, res) => {
 });
 
 // initial work to create an order. a random orderId is generated on the frontend and passed through
-app.post('/api/order/:orderId', async (req, res) => {
+app.post("/api/order/:orderId", async (req, res) => {
   try {
-    console.log('Req', req.body);
-    console.log('Trying');
+    console.log("Req", req.body);
+    console.log("Trying");
 
     // JSON.stringify(req.params.order),
     // 'RGS Nordic',
@@ -48,12 +48,12 @@ app.post('/api/order/:orderId', async (req, res) => {
 
     const orderResult = await invoke.createOrder(
       JSON.stringify({ orderId: req.params.orderId }),
-      'RGS Nordic',
-      JSON.stringify({ access: [''] })
+      "RGS Nordic",
+      JSON.stringify({ access: [""] })
       // JSON.stringify({ access: req.params.orderId.access })
     );
-    console.log('Body', req.body);
-    console.log('OrderResult', orderResult);
+    console.log("Body", req.body);
+    console.log("OrderResult", orderResult);
 
     res.send({
       error: false,
