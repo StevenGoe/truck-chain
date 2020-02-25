54ade81f19adc16073c56f49c9920062a20eacf9
Goehler <Steven.Goehler@itelligence.dk>
2020-02-25 21:41:25 +0100

    Enroll user can now be called from server side with url + /api/user/enroll

--- makeBundle.ps1
+++ makeBundle.ps1
@@ -2,8 +2,11 @@ echo Bundling..
 rm -Recurse -Force deploy
 
 mkdir -p deploy
+cp -r truckchain-frontend/wallet deploy
 cp -r truckchain-frontend/build deploy
 cp -r truckchain-backend/*.js deploy
 cp -r truckchain-backend/node_modules deploy
 cp -r truckchain-backend/package.json deploy
+cp truckchain-backend/connection.json deploy
 cp manifest.yml deploy
+cf push -f manifest.yml -p deploy
